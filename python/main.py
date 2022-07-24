bcb73389669fb636d6de764ac5d555bb9a3bdbb2
cmarius46 <cmarius46@gmail.com>
2022-07-24 14:31:45 +0300

    switched to pose detection

--- python/main.py
+++ python/main.py
@@ -1,27 +1,4 @@
-import cv2
-from cvzone.HandTrackingModule import HandDetector
+from pose_detection import detect_pose
 
-cap = cv2.VideoCapture(0)
-
-detector = HandDetector(detectionCon=0.8, maxHands = 2)
-
-while True:
-	success, img = cap.read()
-	hands, img = detector.findHands(img)
-
-
-	if hands:
-		hand1 = hands[0]
-		lmList1 = hand1['lmList'] # 21 landmark points
-		bbox1 = hand1['bbox'] # bounding box info x, y, w, h
-		centerPoint = hand1['center'] # center of hand
-		handType1 = hand1['type'] # left or right
-
-		print(lmList1[0])
-		print()
-		# print(bbox1)
-		# print(centerPoint)
-
-
-	cv2.imshow("Image", img)
-	cv2.waitKey(1)
+# detect_hands()
+detect_pose()
